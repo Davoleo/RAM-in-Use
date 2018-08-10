95c860791a9e4907158e25fac7ebf3bb397b5d1c
Davoleo <davoleo2001@gmail.com>
2018-08-10 22:49:15 +0200

    First Commit

---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Const WM_NCHITTEST As Integer = &H84
+    Const HTCLIENT As Integer = &H1
+    Const HTCAPTION As Integer = &H2
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        Select Case m.Msg
+            Case WM_NCHITTEST
+                MyBase.WndProc(m)
+                If m.Result = HTCLIENT Then m.Result = HTCAPTION
+            Case Else
+                MyBase.WndProc(m)
+        End Select
+    End Sub
+    Dim X, Y As Integer
+    Dim newpoint As New System.Drawing.Point
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Me.BackColor = Color.Black
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As System.EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value = PerformanceCounter1.NextValue
+        Label2.Text = ProgressBar1.Value.ToString + "%"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
